b08e2dbb031c4f5206221c187c5d979262a88c12
Michael Mathieu <mmathieu757@gmail.com>
2019-10-02 16:17:37 -0400

    mm/baseFragment (#7)
    * added basefragment. Not building for some reason
    
    * working
    
    * working
    
    * working

---
+++ Android/app/src/main/java/com/example/players/BaseFragment.kt
@@ -0,0 +1,18 @@
+package com.example.players
+
+import androidx.fragment.app.Fragment
+
+open class BaseFragment<VM : BaseAndroidVM<*>> : Fragment() {
+
+    protected lateinit var viewModel: VM
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.onAppeared()
+    }
+
+    override fun onPause() {
+        viewModel.onDisappeared()
+        super.onPause()
+    }
+}
