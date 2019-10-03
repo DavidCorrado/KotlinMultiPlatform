eea9d348786c64a4b6b9712d16087be1d6f92d35
Oscar Arnaiz <oscar.arnaiz@fanduel.com>
2019-10-03 12:26:03 +0100

    accept the experimental status without propagation

--- Android/SharedCode/src/commonMain/kotlin/main/MainVM.kt
+++ Android/SharedCode/src/commonMain/kotlin/main/MainVM.kt
@@ -4,26 +4,32 @@ import com.example.players.BaseViewModel
 import com.example.players.api.PlayersAPI
 import com.example.players.live_data.KLiveData
 import com.example.players.live_data.KMutableLiveData
+import com.example.players.model.Player
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.UnstableDefault
 
 class MainVM(private val playersAPI: PlayersAPI) : BaseViewModel() {
 
-    private val _name = KMutableLiveData<String>()
+    private val _players = KMutableLiveData<List<Player>>()
 
-    val name: KLiveData<String> = _name
+    val players: KLiveData<List<Player>> = _players
 
+
+    @UseExperimental(UnstableDefault::class)
     override fun onAppeared() {
         super.onAppeared()
 
         launch(Main) {
             try {
                 val result = withContext(Dispatchers.Default) { playersAPI.fetchPlayer() }
-                _name.value = result.players[0].name + " - " + result.players[1].name + " - " + result.players[2].name + " - " + result.players[3].name
+                _players.value = result.players
+
+
             } catch (e: Exception) {
-                _name.value = e.message
+                println(e.message)
             }
         }
     }
