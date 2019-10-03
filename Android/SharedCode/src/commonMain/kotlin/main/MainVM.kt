979f24022aa3fbc7bcbf0873ab767b40e4254f75
Oscar Arnaiz <oscar.arnaiz@fanduel.com>
2019-10-03 11:59:06 +0100

    Parser for list of player

--- Android/SharedCode/src/commonMain/kotlin/main/MainVM.kt
+++ Android/SharedCode/src/commonMain/kotlin/main/MainVM.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.withContext
 class MainVM(private val playersAPI: PlayersAPI) : BaseViewModel() {
 
     private val _name = KMutableLiveData<String>()
+
     val name: KLiveData<String> = _name
 
     override fun onAppeared() {
@@ -20,7 +21,7 @@ class MainVM(private val playersAPI: PlayersAPI) : BaseViewModel() {
         launch(Main) {
             try {
                 val result = withContext(Dispatchers.Default) { playersAPI.fetchPlayer() }
-                _name.value = result.name
+                _name.value = result.players[0].name + " - " + result.players[1].name + " - " + result.players[2].name + " - " + result.players[3].name
             } catch (e: Exception) {
                 _name.value = e.message
             }
