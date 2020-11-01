fd6cf859153a9063f1132c7eed82f1ee6d5e77e1
davidcorrado <davidcorrado@gmail.com>
2020-11-01 14:34:13 -0600

    cleanup

--- shared/src/iosTest/kotlin/com/davidcorrado/todo/shared/iosTest.kt
+++ shared/src/iosTest/kotlin/com/davidcorrado/todo/shared/iosTest.kt
@@ -1,12 +1,1 @@
 package com.davidcorrado.todo.shared
-
-import kotlin.test.Test
-import kotlin.test.assertTrue
-
-class IosGreetingTest {
-
-    @Test
-    fun testExample() {
-        assertTrue(Greeting().greeting().contains("iOS"), "Check iOS is mentioned")
-    }
-}
